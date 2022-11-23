a21da4dbcc885ec362b7e4b43891dda7dda3da23
antymijaljevic <antymijaljevic@gmail.com>
2022-11-23 06:52:25 +0100

    ok

--- databases_and_analytics/provider.tf
+++ databases_and_analytics/provider.tf
@@ -10,5 +10,8 @@ terraform {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
 
 provider "aws" {
-  profile = "default"
+  region = "us-east-1"
 }
+
+# export AWS_ACCESS_KEY_ID=
+# export AWS_SECRET_ACCESS_KEY=
